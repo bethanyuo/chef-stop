7443d65713d48e0c0cdaec7754bed12fd3178816
bethanyuo <bethanyosueke@gmail.com>
2021-06-05 13:10:03 -0500

    main components

--- src/index.js
+++ src/index.js
@@ -1,14 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './routes/App';
+import Wallet from './routes/Wallet';
+import Cookbook from './routes/Cookbook';
+import Recipe from './routes/Recipe';
+import Navbar from './routes/Navbar';
+import {
+  BrowserRouter,
+  Route,
+  Switch
+} from 'react-router-dom';
+
 import reportWebVitals from './reportWebVitals';
 
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <BrowserRouter>
+  <Navbar />
+    <Switch>
+      <Route exact path='/' render={props => <App {...props} />} />
+      <Route exact path='/wallet' render={props => <Wallet {...props} />} />
+      <Route exact path='/cookbook' render={props => <Cookbook {...props} />} />
+      <Route exact path='/recipes' render={props => <Recipe {...props} />} />
+    </Switch>
+  </BrowserRouter>
+  ,
+  document.getElementById( 'root' )
 );
 
 // If you want to start measuring performance in your app, pass a function
